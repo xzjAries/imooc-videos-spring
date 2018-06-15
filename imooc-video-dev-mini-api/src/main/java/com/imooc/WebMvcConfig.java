23d931336c389f7ef26eb1a513b60212536b7a9f
xuzhijie <1050077383@qq.com>
2018-06-15 18:14:38 +0800

    虚拟目录

--- imooc-video-dev-mini-api/src/main/java/com/imooc/WebMvcConfig.java
+++ imooc-video-dev-mini-api/src/main/java/com/imooc/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-	@Override
+	@Override 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**") 
 		.addResourceLocations("classpath:/META-INF/resources/")
